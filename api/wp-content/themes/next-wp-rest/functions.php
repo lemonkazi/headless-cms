d2cf327a1cfa6fdbf498581317173fcd8191f604
lemonkazi <lemonpstu09@gmail.com>
2023-09-26 22:50:58 +0600

    Initial commit

--- api/wp-content/themes/next-wp-rest/functions.php
+++ api/wp-content/themes/next-wp-rest/functions.php
@@ -4,12 +4,18 @@ require dirname( __FILE__ ) . '/vendor/autoload.php';
 require dirname( __FILE__ ) . '/src/Init.php';
 
 add_action('graphql_register_types', function () {
-    register_graphql_field('Post', 'elementorData', [
-        'type' => 'String',
-        'discription' => __('Elementor Data JSON', 'wp-graphql'),
-        'resolve' => function ($post) {
-            $data = get_post_meta($post->ID, '_elementor_data', true);
-            return !empty($data) ? $data : null;
-        }
-    ]);
+    // Register the 'elementorData' field for both 'Post' and 'Page' types
+    $types_to_add_field_to = ['Post', 'Page'];
+
+    foreach ($types_to_add_field_to as $type) {
+        register_graphql_field($type, 'elementorData', [
+            'type' => 'String',
+            'description' => __('Elementor Data JSON', 'wp-graphql'),
+            'resolve' => function ($post_or_page) use ($type) {
+                $data = get_post_meta($post_or_page->ID, '_elementor_data', true);
+                return !empty($data) ? $data : null;
+            }
+        ]);
+    }
+
 });
