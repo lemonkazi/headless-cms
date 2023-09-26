ac95c44a3f25524803730a2690fd9e86379133cb
lemonkazi <lemonpstu09@gmail.com>
2023-09-26 12:16:07 +0600

    Initial commit

--- api/wp-content/themes/next-wp-rest/functions.php
+++ api/wp-content/themes/next-wp-rest/functions.php
@@ -2,3 +2,14 @@
 
 require dirname( __FILE__ ) . '/vendor/autoload.php';
 require dirname( __FILE__ ) . '/src/Init.php';
+
+add_action('graphql_register_types', function () {
+    register_graphql_field('Post', 'elementorData', [
+        'type' => 'String',
+        'discription' => __('Elementor Data JSON', 'wp-graphql'),
+        'resolve' => function ($post) {
+            $data = get_post_meta($post->ID, '_elementor_data', true);
+            return !empty($data) ? $data : null;
+        }
+    ]);
+});
