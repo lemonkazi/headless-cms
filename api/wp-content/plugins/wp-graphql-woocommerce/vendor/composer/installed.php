0dd4b4455b9c4d330796a8105e658de2b1ec71d3
lemonkazi <lemonpstu09@gmail.com>
2023-10-12 16:45:37 +0600

    Feat(getsby): :pencil: test getsby

---
+++ api/wp-content/plugins/wp-graphql-woocommerce/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wp-graphql/wp-graphql-woocommerce',
+        'pretty_version' => 'v0.18.1',
+        'version' => '0.18.1.0',
+        'reference' => '570d582306cc302493e38e90ce3930b3b7d1d946',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'firebase/php-jwt' => array(
+            'pretty_version' => 'v6.9.0',
+            'version' => '6.9.0.0',
+            'reference' => 'f03270e63eaccf3019ef0f32849c497385774e11',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../firebase/php-jwt',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wp-graphql/wp-graphql-woocommerce' => array(
+            'pretty_version' => 'v0.18.1',
+            'version' => '0.18.1.0',
+            'reference' => '570d582306cc302493e38e90ce3930b3b7d1d946',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
